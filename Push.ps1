80768e26df386292d5fd1800a6a94b2eef21512e
Andrew Piper <andrew@threax.com>
2021-07-21 15:16:32 -0400

    Added push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/htmlrapier.roleclient.git
+git push https://github.com/threax/htmlrapier.roleclient.git --tags
+git push https://gitlab.com/threax-software/htmlrapier.roleclient.git
+git push https://gitlab.com/threax-software/htmlrapier.roleclient.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/htmlrapier.roleclient
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/htmlrapier.roleclient --tags
